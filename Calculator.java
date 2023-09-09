05a4777deb0ccd9301cfcfdaa4c3b10e828f20c4
N.Soma sekhar <143417781+nssekhar@users.noreply.github.com>
2023-09-09 18:54:58 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Simple Calculator");
+        System.out.println("-----------------");
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        double result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero!");
+                    System.exit(1); 
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+                System.exit(1); 
+        }
+        System.out.println("Result: " + result);
+        scanner.close();
+    }
+}
